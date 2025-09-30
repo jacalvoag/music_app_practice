58f94ee815e2ca04d9918083c825f95a5594528d
Jose Calvo <jacalvoaguilar@gmail.com>
2025-09-30 12:42:37 -0600

    refactorizacion de audio controller, player y player interface

--- app/player.js
+++ app/player.js
@@ -1,9 +1,28 @@
-import audioController from "../domain/audio_controller";
+import audioController from "../domain/audio_controller.js";
+import createPlaylist from "../utils/playlist.js";
+import playerInterface from "./player_interface.js";
 
 const player = {
-    
     _last: document.getElementById("lastest"),
-    _next: document.getElementById("foward"),
+    _next: document.getElementById("forward"),
     _play: document.getElementById("play"),
-    _controller: audioController
+    _controller: audioController,
+    _interface: playerInterface,
+    initializePlayer(songs) {
+        this._controller.initializePlayer(
+
+            createPlaylist(songs, null)
+
+        );
+        this._interface.loadSong(this._controller._actualSong);
+        this._last.addEventListener('click', function(){
+            this._controller.nextSong();
+            _interface.loadSong(this._controller._actualSong);
+        });
+        this._next.addEventListener('click', this._controller.nextSong);
+        this._play.addEventListener('click', this._controller.playPause);
+    }
+
 }
+
+export default player;
--- domain/audio_controller.js
+++ domain/audio_controller.js
@@ -1,11 +1,12 @@
 const audioController = {
-    _nextSongs: [],
-    _lastSongs: [],
+    _nextSongs: null,
+    _lastSongs: null,
     _actualSong: null,
     _progress_bar: document.getElementById("progress"),
     _controller: document.getElementById("media"),
     initializePlayer(playlist) {
         this._nextSongs = playlist;
+        this._lastSongs = new Array();
         this._controller.addEventListener('timeupdate', function () {
             player._progress_bar.value = (this.currentTime / this.duration) * 100;
         });
@@ -16,6 +17,19 @@ const audioController = {
     },
     loadSong: function () {
         this._controller.src = this._actualSong.song_url;
+    },
+    nextSong: function() {
+        this._lastSongs.push(this._actualSong);
+        this._actualSong = this._nextSongs.pop();
+        this.loadSong();
+    },
+    lastSong: function() {
+        this._nextSongs.push(this._actualSong);
+        this._actualSong = this._lastSongs.pop();
+        this.loadSong();
+    },
+    playPause(){
+
     }
 }
 
